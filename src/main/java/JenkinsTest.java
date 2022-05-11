1da9726a2d6e65a44134d621e04453d4f728ad4c
Rafal R <rafalrogalski84@gmail.com>
2022-05-11 14:22:52 +0200

    Initial commit

---
+++ src/main/java/JenkinsTest.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class JenkinsTest {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println(LocalDateTime.now());
+    }
+}
+g
